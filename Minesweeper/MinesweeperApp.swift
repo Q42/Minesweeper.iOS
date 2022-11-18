7484882a8356f57f73324fd67e68db159522a581
Mathijs Bernson <mathijsb@q42.nl>
2022-11-18 13:00:55 +0100

    Initial Commit

---
+++ Minesweeper/MinesweeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  MinesweeperApp.swift
+//  Minesweeper
+//
+//  Created by Mathijs Bernson on 18/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct MinesweeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
