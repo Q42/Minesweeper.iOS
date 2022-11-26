ecc64e4630fb4b899a9f02e3f43d0b0f5d8fdd81
Mathijs Bernson <mathijsb@q42.nl>
2022-11-26 16:16:29 +0100

    Remove launch performance tests

--- MinesweeperUITests/MinesweeperUITests.swift
+++ MinesweeperUITests/MinesweeperUITests.swift
@@ -54,13 +54,4 @@ final class MinesweeperUITests: XCTestCase {
             XCTAssertEqual(tile.label, expectedLabel)
         }
     }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
-    }
 }
